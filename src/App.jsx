b219b9707dcd7d31c6ecfdc959fa9904e58935af
a.suchkova <suchkovalika065@gmail.com>
2024-01-19 15:42:19 +0400

    add sum distance & graph

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 import CurrencyDataTable from './components/CurrencyDataTable'
 import FileUploader from './components/FileUploader'
 import JoggingDataTable from './components/JoggingDataTable'
+import JoggingGraph from './components/JoggingGraph'
+import { distanceSum } from './utils/calculateWeekendDistance'
 
 function App() {
 	const [currencyData, setCurrencyData] = useState([])
@@ -15,6 +17,10 @@ function App() {
 				setJoggingData={setJoggingData}
 			/>
 			<CurrencyDataTable data={currencyData} />
+			{joggingData.length != 0 && <JoggingGraph data={joggingData} />}
+			<p>
+				Количество км в выходные = <span>{distanceSum(joggingData)}</span>
+			</p>
 			<JoggingDataTable data={joggingData} />
 		</>
 	)
